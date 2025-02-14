5faef6a9588e676d206f49f041fd6c33783577ee
Antonio <adelarosariquelme1999@gmail.com>
2025-02-14 14:00:52 +0100

    Agregar posición dinámica al helicóptero y establecer su estilo como absoluto

--- helicopter.js
+++ helicopter.js
@@ -4,6 +4,12 @@ function showHelicopter() {
 
     let hel=document.createElement('div'); 
     hel.id="helicopter"; 
+
+    hel.style.top=`${y}px`//coordenada Y
+    hel.style.left=`${x}px`//coordenada X
+
+
+
     main.appendChild(hel);
     
 }
