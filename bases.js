59b6b69345fccbf08ba6d900956e62c5c8b30d9f
Desirée <desireedomingoparejo@gmail.com>
2025-02-17 13:52:40 +0100

    base2.0

--- bases.js
+++ bases.js
@@ -1,8 +1,19 @@
+const basesGeneration = [
+    [0.04 , 0.1],
+    [0.2 , 0.8],
+    [0.9 , 0.2],
+    [0.8 , 0.7],
+]
+
 // Se cogen todas las bases y se muestran cuando se le da al boton start
 function showBases() {
-    var bases = document.getElementsByClassName("base"); 
+    for (let i = 0; i<4; i++){
+        let base = document.createElement('div')
+        base.className = 'base';
+        base.id = 'base' + i;
+        base.style.left = display.width() * basesGeneration[i][0] + 'px';
+        base.style.top = display.height() * basesGeneration[i][1] + 'px';
 
-    for (var i = 0; i < bases.length; i++) {
-        bases[i].style.display = "block"; 
+        main.appendChild(base);
     }
 }
