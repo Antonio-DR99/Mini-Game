5756b44288b017b72c292740b288ebca82581ad4
Rocio Arias Ocón <rao24gr034@student.esgerencia.com>
2025-02-17 13:28:50 +0100

    aparicion de frutas

--- food.js
+++ food.js
@@ -1,35 +1,18 @@
+let fruitCount=0;
+
 //Genera la comida en una posición aleatoria
 function foodGenerate() {
-
-    for (let i=0; i<3; i++) {
     let food = document.createElement('div');
-    food.id="food";
+    food.id="food" + fruitCount;
+    food.classList.add("foodDiv")
+    fruitCount++;
 
     let foodPosition = generateValidCoordsFor('food');
-    food.style.top = foodPosition[0] + 'px';
-    food.style.left = foodPosition[1] + 'px';
+    food.style.top = foodPosition[1] + 'px';
+    food.style.left = foodPosition[0] + 'px';
 
     main.appendChild(food);
     displayedEntities.food.push(food);
-    }
-    
-    foodRot(food);
+
+    setTimeout(foodGenerate,rngSeconds(1, 3));
 }
-
-//Hace desaparecer la comida
-function foodRot(food){
-    let desaparicion = Math.random() * (10000 - 6000) + 6000;
-
-    setTimeout(function () {
-        
-        for (let i = 0; i < displayedEntities.food.length; i++) {
-            displayedEntities.food[i].remove(); 
-        }
-        
-        displayedEntities.food = [];
-    }, desaparicion);
-}
-
-//Despues de un tiempo vuelve a generar comida
-let aparicion = Math.random() * (16000 - 12000) + 12000;
-setTimeout(foodGenerate, aparicion);
