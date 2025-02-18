6df6b09611eeaf2490d8f2cde963cee759ce98b8
Rocio Arias Ocón <rao24gr034@student.esgerencia.com>
2025-02-18 10:19:12 +0100

    vector asociativo comida

--- food.js
+++ food.js
@@ -1,15 +1,22 @@
 let fruitCount=0;
+let foods = []
 
 //Genera la comida en una posición aleatoria
 function foodGenerate() {
     let food = document.createElement('div');
     food.id="food" + fruitCount;
     food.classList.add("food")
-
-    fruitCount++;
-
+    
     let foodType = Math.floor(Math.random()*3)
     food.classList.add("type" + foodType)
+
+    foods.push({
+        'id': fruitCount,
+        'life': 900,
+        'type': foodType,
+    })
+
+    fruitCount++;
 
     let foodPosition = generateValidCoordsFor('food');
     food.style.top = foodPosition[1] + 'px';
@@ -19,4 +26,5 @@ function foodGenerate() {
 
     setTimeout(foodGenerate,rngSeconds(3, 6));
 }
+
 
