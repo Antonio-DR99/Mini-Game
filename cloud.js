568f5192c5dcf1bf031a4ec05764d985d14f8fc0
Antonio <adelarosariquelme1999@gmail.com>
2025-02-19 15:44:08 +0100

    Añadir rotación a las nubes al rebotar y al randomizar la velocidad

--- cloud.js
+++ cloud.js
@@ -6,6 +6,7 @@ const cloud = {
     speedY: 2,
     attacking: false,
     count: 0,
+    angle:0,
 
     show() {
         let stormCloud = document.createElement("div");
@@ -19,29 +20,43 @@ const cloud = {
     },
 
     move() {
+        let rebote=false;
+
         cloud.posX += cloud.speedX;
         cloud.posY += cloud.speedY;
         
         // Invertir dirección si toca los bordes
         if (cloud.posX <= 0 || cloud.posX >= display.width() - cloud.size) {
             cloud.speedX = -cloud.speedX; // Cambiar la dirección
+            rebote=true;
         }
     
         if (cloud.posY <= 0 || cloud.posY >= display.height() - cloud.size) {
             cloud.speedY = -cloud.speedY;
+            rebote=true;
         }
-    
+        
+        if (rebote){
+            cloud.updateRotation();
+        }
+
         // Actualizar posición
         stormCloud.style.left = cloud.posX + "px";
         stormCloud.style.top = cloud.posY + "px";
+        stormCloud.style.transform=`rotate(${cloud.angle}deg)`;
 
         cloud.count++
         if(cloud.count >= 500){
             cloud.count = 0
             cloud.randomize()
         }
+
     },
 
+    updateRotation() {
+        // Calcular ángulo basado en la dirección de movimiento
+        cloud.angle = Math.atan2(cloud.speedY, cloud.speedX) * (180 / Math.PI);
+    },
     checkCollision() {
         let helicopter = document.getElementById("helicopter");
         let heliX = helicopter.offsetLeft;
@@ -58,6 +73,7 @@ const cloud = {
         if(Math.random() < 0.6){
             cloud.speedX = ((Math.random()*3)+1)*rngMathInvert()
             cloud.speedY = ((Math.random()*3)+1)*rngMathInvert()
+            cloud.updateRotation();
         }
     }
 }
